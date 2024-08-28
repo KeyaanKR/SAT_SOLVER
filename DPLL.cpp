47c3dbae516545c162ecc29884ad8ffff5e7d28b
KeyaanKR <keyaan.edu@gmail.com>
2024-08-28 09:40:44 +0530

    dpll corrected
    now, the dpll can take string literals

--- DPLL.cpp
+++ DPLL.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
+#include <sstream>
 
 class DPLL {
 private:
-  std::set<char> assign_t, assign_f;
+  std::set<std::string> assign_t, assign_f;
   int n_prop = 0, n_decs = 0;
 
   void print_cnf(const std::vector<std::string> &cnf) {
@@ -22,18 +23,65 @@ private:
   }
 
   bool is_unit_clause(const std::string &clause) {
-    // check for whitespace, if found then not a unit clause
-    if (clause.length() > 0 &&
-        std::find(clause.begin(), clause.end(), ' ') != clause.end())
-      return false;
-    return true;
+    std::istringstream iss(clause);
+    std::vector<std::string> literals;
+    std::string literal;
+
+    while (iss >> literal)
+      literals.push_back(literal);
+
+    return literals.size() == 1;
   }
 
-  bool solve(std::vector<std::string> cnf, std::set<char> literals) {
-    std::cout << "\nCNF = ";
-    print_cnf(cnf);
+  void removeUnitfromClauses(std::vector<std::string> &cnf,
+                             const std::string &unit) {
+    for (auto &clause : cnf) {
+      std::istringstream iss(clause);
+      std::vector<std::string> words;
+      std::string word;
 
-    std::vector<char> new_t, new_f;
+      // Split clause into words
+      while (iss >> word) {
+        if (word != unit) {
+          words.push_back(word); // Only keep the word if it's not the unit
+        }
+      }
+
+      // Join the remaining words back into the clause
+      std::ostringstream oss;
+      for (size_t i = 0; i < words.size(); ++i) {
+        if (i > 0) {
+          oss << " "; // Add space between words
+        }
+        oss << words[i];
+      }
+
+      // Update the clause with the modified content
+      clause = oss.str();
+    }
+  }
+
+  void deleteClause(std::vector<std::string> &cnf, const std::string &unit) {
+    cnf.erase(std::remove_if(cnf.begin(), cnf.end(),
+                             [&unit](const std::string &clause) {
+                               std::istringstream iss(clause);
+                               std::string word;
+                               while (iss >> word) {
+                                 // Check if the exact unit is in the clause
+                                 if (word == unit) {
+                                   return true; // Mark for removal
+                                 }
+                               }
+                               return false; // Not marked for removal
+                             }),
+              cnf.end()); // Remove clauses that contain the unit
+  }
+
+  bool solve(std::vector<std::string> cnf, std::set<std::string> literals) {
+    // std::cout << "\nCNF = ";
+    // print_cnf(cnf);
+
+    std::vector<std::string> new_t, new_f;
     n_decs++;
 
     // remove duplicate clauses
@@ -42,8 +90,9 @@ private:
 
     // find unit clauses
     std::vector<std::string> units;
-    std::copy_if(cnf.begin(), cnf.end(), std::back_inserter(units),
-                 [](const std::string &clause) { return clause.length() < 3; });
+    std::copy_if(
+        cnf.begin(), cnf.end(), std::back_inserter(units),
+        [this](const std::string &clause) { return is_unit_clause(clause); });
 
     // if there are unit clauses
     if (!units.empty()) {
@@ -51,43 +100,28 @@ private:
         n_prop++;
         // if the unit is a negation
         if (unit[0] == '~') {
-          new_f.push_back(unit[1]);
-          assign_f.insert(unit[1]);
-          cnf.erase(std::remove(cnf.begin(), cnf.end(), unit), cnf.end());
+          new_f.push_back(unit.substr(1));
+          assign_f.insert(unit.substr(1));
+          // remove the clauses with the unit
+          deleteClause(cnf, unit);
           // remove the negation from clauses
-          for (auto &clause : cnf) {
-            auto pos = clause.find(unit[1]);
-            // if the negation is found, remove it
-            if (pos != std::string::npos) {
-              clause.erase(pos, 1);
-              clause.erase(std::remove(clause.begin(), clause.end(), ' '),
-                           clause.end());
-            }
-          }
+          removeUnitfromClauses(cnf, unit.substr(1));
         } else {
-          new_t.push_back(unit[0]);
-          assign_t.insert(unit[0]);
+          new_t.push_back(unit);
+          assign_t.insert(unit);
+          // remove the clauses with the unit
+          deleteClause(cnf, unit);
           // remove the unit from the clauses
-          for (auto it = cnf.begin(); it != cnf.end();) {
-            auto pos = it->find("~" + unit);
-            if (pos != std::string::npos) {
-              it->erase(pos, 2);
-              it->erase(std::remove(it->begin(), it->end(), ' '), it->end());
-            } else if (it->find(unit) != std::string::npos) {
-              it = cnf.erase(it);
-              continue;
-            }
-            it++;
-          }
+          removeUnitfromClauses(cnf, '~' + unit);
         }
       }
     }
 
-    std::cout << "units = ";
-    for (const auto &unit : units)
-      std::cout << unit << " ";
-    std::cout << "\nCNF after unit propagation = ";
-    print_cnf(cnf);
+    // std::cout << "units = ";
+    // for (const auto &unit : units)
+    //   std::cout << unit << " ";
+    // std::cout << "\nCNF after unit propagation = ";
+    // print_cnf(cnf);
 
     // if the cnf is empty, return true
     if (cnf.empty())
@@ -96,39 +130,44 @@ private:
     if (std::any_of(cnf.begin(), cnf.end(),
                     [](const std::string &clause) { return clause.empty(); })) {
       // remove the assignments and backtrack
-      for (char literal : new_t)
+      for (std::string literal : new_t)
         assign_t.erase(literal);
-      for (char literal : new_f)
+      for (std::string literal : new_f)
         assign_f.erase(literal);
-      std::cout << "Null clause found, backtracking..." << std::endl;
+      // std::cout << "Null clause found, backtracking..." << std::endl;
       return false;
     }
 
     literals.clear();
     for (const auto &clause : cnf) {
-      for (char c : clause) {
-        if (std::isalpha(c) || std::isdigit(c))
-          literals.insert(c);
+      std::istringstream iss(clause);
+      std::string literal;
+
+      while (iss >> literal) {
+        if (literal[0] == '~')
+          literals.insert(literal.substr(1));
+        else
+          literals.insert(literal);
       }
     }
 
     if (!literals.empty()) {
-      char literal = *literals.begin();
+      std::string literal = *literals.begin();
       std::vector<std::string> new_cnf = cnf;
-      new_cnf.push_back(std::string(1, literal));
+      new_cnf.push_back(literal);
 
       if (solve(new_cnf, literals))
         return true;
 
       new_cnf = cnf;
-      new_cnf.push_back("~" + std::string(1, literal));
+      new_cnf.push_back("~" + literal);
       if (solve(new_cnf, literals))
         return true;
     }
 
-    for (char literal : new_t)
+    for (std::string literal : new_t)
       assign_t.erase(literal);
-    for (char literal : new_f)
+    for (std::string literal : new_f)
       assign_f.erase(literal);
     return false;
   }
@@ -142,14 +181,19 @@ public:
     }
 
     std::vector<std::string> cnf;
-    std::set<char> literals;
+    std::set<std::string> literals;
     std::string line;
 
     while (std::getline(file, line)) {
       cnf.push_back(line);
-      for (char c : line) {
-        if (std::isalpha(c) || std::isdigit(c))
-          literals.insert(c);
+      std::istringstream iss(line);
+      std::string literal;
+
+      while (iss >> literal) {
+        if (literal[0] == '~')
+          literals.insert(literal.substr(1));
+        else
+          literals.insert(literal);
       }
     }
 
@@ -158,9 +202,9 @@ public:
       std::cout << "Unit Propagations = " << n_prop << std::endl;
       std::cout << "\nResult: SATISFIABLE" << std::endl;
       std::cout << "Solution:" << std::endl;
-      for (char literal : assign_t)
+      for (std::string literal : assign_t)
         std::cout << "\t\t" << literal << " = True" << std::endl;
-      for (char literal : assign_f)
+      for (std::string literal : assign_f)
         std::cout << "\t\t" << literal << " = False" << std::endl;
     } else {
       std::cout << "\nReached starting node!" << std::endl;
